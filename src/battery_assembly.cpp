74ba8bec5073c235ab4128c3292af46c8ab73fb3
DavyVos <davy_vos@outlook.com>
2024-06-05 15:26:37 +0200

    hello world example

--- src/battery_assembly.cpp
+++ src/battery_assembly.cpp
@@ -1,10 +1,23 @@
-#include <cstdio>
+#include <memory>
 
-int main(int argc, char ** argv)
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.h>
+
+int main(int argc, char * argv[])
 {
-  (void) argc;
-  (void) argv;
+  // Initialize ROS and create the Node
+  rclcpp::init(argc, argv);
+  auto const node = std::make_shared<rclcpp::Node>(
+    "hello_moveit",
+    rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)
+  );
 
-  printf("hello world battery_assembly_planner package\n");
+  // Create a ROS logger
+  auto const logger = rclcpp::get_logger("hello_moveit");
+
+  // Next step goes here
+
+  // Shutdown ROS
+  rclcpp::shutdown();
   return 0;
 }
